5b7da3ba880c99cec7c40238a0ca93389fc8c008
Marijn Ophorst <marijn@sensimedia.nl>
2016-07-15 12:21:02 +0200

    tests

---
+++ tests/schema.sql
@@ -0,0 +1,25 @@
+
+CREATE SEQUENCE IF NOT EXISTS test_id_seq;
+CREATE TABLE test (
+    id INTEGER NOT NULL DEFAULT NEXTVAL('test_id_seq'),
+    bar INTEGER NOT NULL,
+    baz VARCHAR(16) NOT NULL DEFAULT 'fizz',
+    foo VARCHAR(255) DEFAULT 'buzz'
+);
+
+CREATE INDEX test_bar_idx ON test(bar);
+
+CREATE FUNCTION test_before_insert() RETURNS "trigger" AS $$
+BEGIN
+    NEW.bar := NEW.bar + 1;
+    RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+CREATE TRIGGER test_before_insert BEFORE INSERT ON test FOR EACH ROW EXECUTE PROCEDURE test_before_insert();
+
+IF NOT EXISTS (SELECT 1 FROM test WHERE id = 1) THEN
+    INSERT INTO test VALUES (1, 2, NULL, 'foo');
+END IF;
+
+CREATE VIEW viewtest AS SELECT * FROM test;
+
