fedfb346a3ee0364c1baacee3d272bc8cc231777
Marijn Ophorst <marijn@monomelodies.nl>
2018-03-25 14:51:05 +0200

    conditionals plugin

---
+++ src/Conditionals.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @package Dbmover
+ * @subpackage Pgsql
+ */
+
+namespace Dbmover\Pgsql;
+
+use Dbmover\Coore;
+
+/**
+ * Gather all conditionals and optionally wrap them in a "lambda".
+ */
+class Conditionals extends Core\Conditionals
+{
+    protected function wrap(string $sql) : string
+    {
+        $database = $this->loader->getDatabase();
+        $tmp = 'tmp_'.md5(microtime(true));
+        return <<<EOT
+DROP FUNCTION IF EXISTS $tmp();
+CREATE FUNCTION $tmp() RETURNS void AS $$
+DECLARE DBMOVER_DATABASE TEXT;
+BEGIN
+    DBMOVER_DATABASE := '$database';
+    $sql
+END;
+$$ LANGUAGE 'plpgsql';
+SELECT $tmp();
+DROP FUNCTION $tmp();
+
+EOT;
+    }
+}
+
