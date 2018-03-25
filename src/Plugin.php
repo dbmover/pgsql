bc05d587c737db5f67ba20ceee065948ab34f0ec
Marijn Ophorst <marijn@monomelodies.nl>
2018-03-25 14:51:52 +0200

    use local plugins

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,26 +3,27 @@
 /**
  * @package Dbmover
  * @subpackage Pgsql
- *
- * Meta-package loading all officially supported PostgreSQL plugins.
  */
 
 namespace Dbmover\Pgsql;
 
 use Dbmover\Core;
 
+/**
+ * Meta-package loading all officially supported PostgreSQL plugins.
+ */
 class Plugin extends Core\Plugin
 {
     public function __construct(Core\Loader $loader)
     {
         parent::__construct($loader);
         $loader->loadPlugins(
-            Procedures\Plugin::class,
-            Views\Plugin::class,
-            Constraints\Plugin::class,
-            Triggers\Plugin::class,
-            Sequences\Plugin::class,
-            Tables\Plugin::class
+            Procedures::class,
+            Views::class,
+            Constraints::class,
+            Triggers::class,
+            Sequences::class,
+            Tables::class
         );
     }
 }
