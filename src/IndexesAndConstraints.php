d9643f38ac2b9f0ca98b86f8a9f8d742da11f762
Marijn Ophorst <marijn@monomelodies.nl>
2019-12-03 15:04:29 +0100

    only (re)create constraints if they are new or have changed

--- src/IndexesAndConstraints.php
+++ src/IndexesAndConstraints.php
@@ -31,24 +31,119 @@ class IndexesAndConstraints extends Core\IndexesAndConstraints
             $sql
         );
         $stmt = $this->loader->getPdo()->prepare(
-            "SELECT c.relname tbl, o.conname constr, o.contype ctype
-                FROM pg_constraint o JOIN pg_class c ON o.conrelid = c.oid
-                WHERE o.contype IN ('f', 'x')"
+            "SELECT kcu.table_name AS foreign_table,
+                rel_kcu.table_name AS primary_table,
+                kcu.column_name AS fk_column,
+                rel_kcu.column_name AS pk_column,
+                kcu.constraint_name,
+                update_rule,
+                delete_rule
+            FROM information_schema.table_constraints tco
+            JOIN information_schema.key_column_usage kcu
+                ON tco.constraint_schema = kcu.constraint_schema AND tco.constraint_name = kcu.constraint_name
+            JOIN information_schema.referential_constraints rco
+                ON tco.constraint_schema = rco.constraint_schema AND tco.constraint_name = rco.constraint_name
+            JOIN information_schema.key_column_usage rel_kcu
+                ON rco.unique_constraint_schema = rel_kcu.constraint_schema
+                    AND rco.unique_constraint_name = rel_kcu.constraint_name
+                    AND kcu.ordinal_position = rel_kcu.ordinal_position
+            WHERE tco.constraint_type = 'FOREIGN KEY'
+            ORDER BY kcu.table_schema, kcu.table_name, kcu.ordinal_position"
         );
         $stmt->execute();
-        while (false !== ($constraint = $stmt->fetch(PDO::FETCH_ASSOC))) {
-            if (!$this->loader->shouldBeIgnored($constraint['constr'])) {
-                $this->dropConstraint($constraint['tbl'], $constraint['constr'], $constraint['ctype']);
+        $existing_fks = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $sql = parent::__invoke($sql);
+        // Get requested foreign keys:
+        $requested_fks = [];
+        foreach ($this->extractOperations("@^ALTER TABLE (?'foreign_table'\S+) ADD FOREIGN KEY\s*\((?'fk_column'.*?)\) REFERENCES (?'primary_table'\S+)\((?'pk_column'\S+)\)( ON UPDATE (?'update_rule'RESTRICT|CASCADE|SET NULL))?( ON DELETE (?'delete_rule'RESTRICT|CASCADE|SET NULL))?;$@ms", $sql) as $match) {
+            foreach (['update_rule', 'delete_rule'] as $rule) {
+                if (!strlen($match[$rule])) {
+                    $match[$rule] = 'NO ACTION';
+                }
+            }
+            foreach ($match as $k => $v) {
+                if (is_numeric($k) && $k) {
+                    unset($match[$k]);
+                }
+            }
+            $requested_fks[] = $match;
+        }
+        foreach ($existing_fks as $fk) {
+            if (!$this->wantsFk($fk, $requested_fks)) {
+                $this->dropConstraint($fk['foreign_table'], $fk['constraint_name'], 'f');
             }
         }
-        $sql = parent::__invoke($sql);
-        foreach ($this->extractOperations("@^ALTER TABLE \S+ ADD FOREIGN KEY.*?;@ms", $sql) as $match) {
-            $this->defer($match[0]);
+        foreach ($requested_fks as $fk) {
+            $generate = array_shift($fk);
+            if (!$this->hasFk($fk, $existing_fks)) {
+                $this->defer($generate);
+            }
         }
-        foreach ($this->extractOperations("@^ALTER TABLE \S+ ADD CONSTRAINT.*?;@ms", $sql) as $match) {
-            $this->defer($match[0]);
+
+        // Now, the constraints...
+        $stmt = $this->loader->getPdo()->prepare("
+            SELECT pgc.conname AS constraint_name,
+                ccu.table_schema AS table_schema,
+                ccu.table_name,
+                ccu.column_name,
+                pgc.consrc as definition
+            FROM pg_constraint pgc
+                JOIN pg_namespace nsp on nsp.oid = pgc.connamespace
+                JOIN pg_class  cls on pgc.conrelid = cls.oid
+                LEFT JOIN information_schema.constraint_column_usage ccu
+                    ON pgc.conname = ccu.constraint_name
+                    AND nsp.nspname = ccu.constraint_schema
+            WHERE contype ='c'
+            ORDER BY pgc.conname"
+        );
+        $stmt->execute();
+        $existing_checks = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $requested_checks = [];
+        foreach ($this->extractOperations("@^ALTER TABLE (?'table_name'\S+) ADD CHECK \((?'definition'.*?)\);@ms", $sql) as $match) {
+            foreach ($existing_checks as $i => $check) {
+                if ($check['table_name'] == $match['table_name']) {
+                    unset($existing_checks[$i]);
+                    continue 2;
+                }
+            }
+            $requested_checks[] = $match;
+        }
+        foreach ($existing_checks as $check) {
+            if (!$this->loader->shouldBeIgnored($check['constraint_name'])) {
+                $this->dropConstraint($check['table_name'], $check['constraint_name'], 'x');
+            }
+        }
+        foreach ($requested_checks as $check) {
+            $this->defer($check[0]);
         }
         return $sql;
+    }
+
+    private function wantsFk(array $fk, array $fks) : bool
+    {
+        ksort($fk);
+        foreach ($fks as $other) {
+            unset($other[0]);
+            ksort($other);
+            if ($fk == $other) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private function hasFk(array $fk, array $fks) : bool
+    {
+        unset($fk[0]);
+        ksort($fk);
+        foreach ($fks as $other) {
+            unset($other['constraint_name']);
+            ksort($other);
+            if ($fk == $other) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
